0f33493e88136c7431b436776eddf1c0874b9e3e
baonv <baonv@kozo-japan.com>
2022-08-11 15:11:26 +0700

    handlebar

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require('express-handlebars')
+const sass = require('node-sass')
+const port = 4000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json())
+
+//http loger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
