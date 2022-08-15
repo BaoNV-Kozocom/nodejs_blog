37924cac1de5026b9dc16fcd13de09e72a307d15
baonv <baonv@kozo-japan.com>
2022-08-15 09:40:57 +0700

    add prettier

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const path = require('path')
-const { engine } = require('express-handlebars')
-const sass = require('node-sass')
-const port = 4000
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+const { engine } = require('express-handlebars');
+const sass = require('node-sass');
+const route = require('./routes');
+const port = 4000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-    extended: true,
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //http loger
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 //template engine
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+//route init
+route(app);
+
 app.get('/', (req, res) => {
-    res.render('home')
-})
+    res.render('home');
+});
 
 app.get('/search', (req, res) => {
-    res.render('search')
-})
+    res.render('search');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
